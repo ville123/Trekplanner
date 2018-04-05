6cea3304cb7826cd36807267254053f7aa45fbd1
Ville N <t6nivi02@students.oamk.fi>
2018-04-06 00:16:14 +0300

    TrekEditFragment editview_trek_content_layout lisäyksiä

--- src/Trekplanner/app/src/main/java/com/trekplanner/app/fragment/editable/TrekEditFragment.java
+++ src/Trekplanner/app/src/main/java/com/trekplanner/app/fragment/editable/TrekEditFragment.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 
 import com.trekplanner.app.R;
 import com.trekplanner.app.db.DbHelper;
@@ -38,6 +41,33 @@ public class TrekEditFragment extends EditFragment {
         EditText startField = parentView.findViewById(R.id.editview_trek_start_fld);
         this.trek.setStart(startField.getText().toString());
 
+
+        EditText descField = parentView.findViewById(R.id.editview_trek_description_fld);
+        this.trek.setDescription(descField.getText().toString());
+
+        EditText endField = parentView.findViewById(R.id.editview_trek_end_fld);
+        this.trek.setEnd(endField.getText().toString());
+
+        EditText notesField = parentView.findViewById(R.id.editview_trek_notes_fld);
+        this.trek.setNotes(notesField.getText().toString());
+
+        EditText lessonField = parentView.findViewById(R.id.editview_trek_lesson_fld);
+        this.trek.setLessonsLearned(lessonField.getText().toString());
+
+        EditText levelField = parentView.findViewById(R.id.editview_trek_level_fld);
+        this.trek.setLevel(levelField.getText().toString());
+
+        EditText startCoordsField = parentView.findViewById(R.id.editview_trek_start_coord_fld);
+        this.trek.setStartCoords(startCoordsField.getText().toString());
+
+        EditText endCoordsField = parentView.findViewById(R.id.editview_trek_end_coord_fld);
+        this.trek.setEndCoords(endCoordsField.getText().toString());
+
+        EditText lengthField = parentView.findViewById(R.id.editview_trek_length_fld);
+        String lengthString = lengthField.getText().toString().trim();
+        Double length = Double.parseDouble(lengthString);
+        this.trek.setLength(length);
+
         // TODO: add all fields
 
         db.saveTrek(this.trek);
@@ -59,8 +89,31 @@ public class TrekEditFragment extends EditFragment {
         EditText endField = view.findViewById(R.id.editview_trek_end_fld);
         endField.setText(trek.getEnd());
 
+        EditText startCoordsField = view.findViewById(R.id.editview_trek_start_coord_fld);
+        startCoordsField.setText(trek.getStartCoords());
+
+        EditText endCoordsField = view.findViewById(R.id.editview_trek_end_coord_fld);
+        endCoordsField.setText(trek.getEndCoords());
+
         EditText notesField = view.findViewById(R.id.editview_trek_notes_fld);
         notesField.setText(trek.getNotes());
+
+        EditText lessonField = view.findViewById(R.id.editview_trek_lesson_fld);
+        lessonField.setText(trek.getLessonsLearned());
+
+        EditText levelField = view.findViewById(R.id.editview_trek_level_fld);
+        levelField.setText(trek.getLevel());
+
+        EditText lengthField = view.findViewById(R.id.editview_trek_length_fld);
+        lengthField.setText(trek.getLength().toString().trim());
+
+        FloatingActionButton myFab = (FloatingActionButton) view.findViewById(R.id.editview_floating_camera_btn);
+        myFab.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
 
         ((AppCompatActivity)this.getActivity()).getSupportActionBar()
                 .setTitle(getResources().getString(R.string.term_trek) + " - " + trek.getDescription());
